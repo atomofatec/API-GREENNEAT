2dcaa16bc309eb8b53a200f4459f27d73a2d4843
João Gabriel <joao.mira@fatec.sp.gov.br>
2023-09-04 13:53:50 -0300

    fixed folders
    estrutura do projeto alterada

--- greenneat-app/frontend/src/index.js
+++ greenneat-app/frontend/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Router from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Router />
   </React.StrictMode>
 );
 
