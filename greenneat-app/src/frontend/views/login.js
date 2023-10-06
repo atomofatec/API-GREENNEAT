f5170cf23c04af964c5634f921730e6ccef3de9a
João Gabriel <joao.mira@fatec.sp.gov.br>
2023-10-06 20:43:10 -0300

    Update login.js

--- greenneat-app/src/frontend/views/login.js
+++ greenneat-app/src/frontend/views/login.js
@@ -22,19 +22,23 @@ import axios from 'axios'
 const defaultTheme = createTheme();
 
 const backgroundColor = {
-    backgroundColor: 'white'
+  backgroundColor: 'white',
 };
 
 const fontColor = {
-    color: '#0E681D'
+  color: '#0E681D',
 };
 
 export default function Login() {
   React.useEffect(() => {
-    localStorage.clear()
-  })
+    localStorage.clear();
+  }, []);
 
-  const [values, setValues] = React.useState();
+  const [values, setValues] = React.useState({
+    email: '',
+    password: '',
+  });
+
   const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = React.useState(false);
@@ -45,79 +49,59 @@ export default function Login() {
     event.preventDefault();
   };
 
-  const handleChangeValues = (value) => {
-    setValues(prevValue => ({
-      ...prevValue,
-      [value.target.name]: value.target.value,
-    }))
+  const checkEmpty = () => {
+    let isVazio = false;
+    if (values.email === '' || values.password === '') {
+      isVazio = true;
+    }
+    return isVazio;
   };
 
-  const checkEmpty = () => {
-    let isVazio = false
-    if (document.getElementById('email').value === '') {
-      isVazio = true
-      return isVazio
-    }
-    if (document.getElementById('password').value === '') {
-      isVazio = true
-      return isVazio
-    }
-  }
+  const handleChangeValues = (event) => {
+    const { name, value } = event.target;
+    setValues((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
 
   const handleClickButton = () => {
     if (!checkEmpty()) {
-      axios.post("http://localhost:3001/", {
-        email: values.email,
-        password: values.password,
-      }).then((response) => {
-        console.log(response.data)
-        if (response.data.msg === "Usuário logado") {
-          localStorage.setItem('user', response.data.id)
-          localStorage.setItem('tipo', response.data.type_user)
-          localStorage.setItem('email', response.data.email)
-          localStorage.setItem('cpf', response.data.cpf)
-          localStorage.setItem('cnpj', response.data.cnpj)
-          localStorage.setItem('balance', response.data.balance)
-          if (response.data.type_user === "supplier") {
-            navigate('/carteira-estabelecimento')
+      axios
+        .post('http://localhost:3001/', {
+          email: values.email,
+          password: values.password,
+        })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.msg === 'Usuário logado') {
+            localStorage.setItem('user', response.data.id);
+            localStorage.setItem('tipo', response.data.type_user);
+            localStorage.setItem('email', response.data.email);
+            localStorage.setItem('cpf', response.data.cpf);
+            localStorage.setItem('cnpj', response.data.cnpj);
+            localStorage.setItem('balance', response.data.balance);
+            if (response.data.type_user === 'supplier') {
+              navigate('/carteira-estabelecimento');
+            } else if (response.data.type_user === 'partner') {
+              navigate('/dashboard-cooperativo');
+            } else if (response.data.type_user === 'admin') {
+              navigate('/dashboard-greenneat');
+            }
+          } else {
+            alert('Não foi possível logar');
           }
-          else if (response.data.type_user === "partner") {
-            navigate('/dashboard-cooperativo')
-          }
-          else if (response.data.type_user === "admin") {
-            navigate('/dashboard-greenneat')
-          }
-        } else {
-          alert('Não foi possível logar')
-        }
-      })
+        });
     } else {
-      alert('Preencha todos os campos')
+      alert('Preencha todos os campos');
     }
-  }
-
-  /*  const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      console.log({
-        email: data.get('email'),
-        password: data.get('password'),
-      });
-    };*/
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: '100vh' }} style={backgroundColor}>
         <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-
-          }}
-        />
+        <Grid item xs={false} sm={4} md={7} sx={{}} />
         <Triangle />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={0} style={backgroundColor}>
           <Box
@@ -131,13 +115,10 @@ export default function Login() {
             }}
           >
             <Typography style={fontColor} fontSize={'2vw'}>
-                <strong>
-                  Seja Bem-Vindo(a)!
-                </strong>
+              <strong>Seja Bem-Vindo(a)!</strong>
             </Typography>
-            {/* onSubmit={handleSubmit} */}
             <Box component="form" noValidate sx={{ mt: 1 }}>
-                <TextField
+              <TextField
                 margin="normal"
                 color="success"
                 fullWidth
@@ -149,6 +130,7 @@ export default function Login() {
                 autoFocus
                 style={{ backgroundColor: 'white' }}
                 onChange={handleChangeValues}
+                value={values.email}
               />
               <FormControl
                 variant="outlined"
@@ -162,7 +144,7 @@ export default function Login() {
                 autoComplete="current-password"
                 required
                 style={{ backgroundColor: 'white' }}
-                onChange={handleChangeValues}>
+              >
                 <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
                 <OutlinedInput
                   id="outlined-adornment-password"
@@ -180,6 +162,9 @@ export default function Login() {
                     </InputAdornment>
                   }
                   label="password"
+                  name="password"
+                  onChange={handleChangeValues}
+                  value={values.password}
                 />
               </FormControl>
               <Button
@@ -187,7 +172,7 @@ export default function Login() {
                 variant="contained"
                 color="success"
                 onClick={handleClickButton}
-                sx={{ mt: 3, mb: 2, backgroundColor: '#0E681D'}}
+                sx={{ mt: 3, mb: 2, backgroundColor: '#0E681D' }}
               >
                 Entrar
               </Button>
