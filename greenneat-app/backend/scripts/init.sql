9dc17d8424da8800162b4f3fc0e064398baf02f3
Simone H. Kanzawa <kanzawasimone@gmail.com>
2023-10-26 14:27:36 -0300

    Feat: update database

--- greenneat-app/backend/scripts/init.sql
+++ greenneat-app/backend/scripts/init.sql
@@ -4,3 +4,14 @@ insert into locations (nameArea) values ('Norte'), ('Sul'), ('Leste'), ('Oeste')
 ALTER TABLE suppliers_oil ADD COLUMN price decimal(10,2);
 ALTER TABLE suppliers_oil ADD COLUMN status varchar(15);
 ALTER TABLE oils DROP COLUMN price;
+
+ALTER TABLE suppliers_oil ADD COLUMN idUserPartner integer;
+ALTER TABLE suppliers_oil
+ADD CONSTRAINT fk_suppliers_oil_idUserPartner
+FOREIGN KEY (idUserPartner)
+REFERENCES users (id);
+
+ALTER TABLE suppliers_oil RENAME COLUMN date TO CollectionDate;
+ALTER TABLE suppliers_oil ADD COLUMN availableDate timestamp;
+ALTER TABLE suppliers_oil ADD COLUMN deliveryDate timestamp;
+ALTER TABLE SUPPLIERS_OIL ALTER COLUMN collectiondate DROP NOT NULL
